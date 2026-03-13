c787f763645f022212930d55e5756d637b83a32d
tjgustjr001-star <tjgustjr001@gmail.com>
2026-03-13 10:11:45 +0900

    A message

---
+++ get_test/src/main/java/com/java/git/A.java
@@ -0,0 +1,11 @@
+package com.java.git;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fd");
+		System.out.println("fd");
+	}
+
+}
